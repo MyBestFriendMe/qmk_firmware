63ee0988b033f0e4fbc38a979eb4627b42ca352d
MyBestFriendMe <codylmcfadden@gmail.com>
2024-11-04 12:20:22 -0600

    minor changes

--- keyboards/handwired/dactyl_cc/5x7/rules.mk
+++ keyboards/handwired/dactyl_cc/5x7/rules.mk
@@ -3,12 +3,12 @@
 #
 BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
 MOUSEKEY_ENABLE = yes       # Mouse keys
-EXTRAKEY_ENABLE = yes       # Audio control and System control
+EXTRAKEY_ENABLE = yes		# Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = yes        # Commands for debug and configuration
+COMMAND_ENABLE = no         # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
-SPLIT_KEYBOARD = yes
+SPLIT_KEYBOARD = yes		# Keyboard has two controllers
 LTO_ENABLE = yes 			# Optimise
