462c405cea2bad58fb7aa2991c7356f9396600c1
MyBestFriendMe <codylmcfadden@gmail.com>
2024-11-12 12:52:29 -0600

    oopsie daisy

--- keyboards/handwired/dactyl_cc/5x7/rev2/rules.mk
+++ keyboards/handwired/dactyl_cc/5x7/rev2/rules.mk
@@ -17,6 +17,7 @@ LTO_ENABLE = yes 			# Optimise
 
 WS2812_DRIVER = vendor
 SERIAL_DRIVER = vendor
+BOARD = GENERIC_RP_RP2040
 
 # If you want to change the display of OLED, you need to change here
 SRC +=  ./lib/rgb_state_reader.c \
