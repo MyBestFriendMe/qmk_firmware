439e74aa03344feeaf74598ab6b4336da2d22dc4
MyBestFriendMe <codylmcfadden@gmail.com>
2024-11-15 18:30:05 -0600

    Update rules.mk

--- keyboards/handwired/dactyl_cc/5x7/rev2/rules.mk
+++ keyboards/handwired/dactyl_cc/5x7/rev2/rules.mk
@@ -1,19 +1,19 @@
 # Build Options
 #   change yes to no to disable
 #
-BOOTMAGIC_ENABLE = yes      # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
+BOOTMAGIC_ENABLE = yes          # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes           # Mouse keys
 EXTRAKEY_ENABLE = yes		# Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
-NKRO_ENABLE = no            # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
-RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
-AUDIO_ENABLE = no           # Audio output
+CONSOLE_ENABLE = no             # Console for debug
+COMMAND_ENABLE = no             # Commands for debug and configuration
+NKRO_ENABLE = no                # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no           # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = yes           # Enable keyboard RGB underglow
+AUDIO_ENABLE = no               # Audio output
 SPLIT_KEYBOARD = yes		# Keyboard has two controllers
 ENCODER_ENABLE = yes		# Encoder Functionality Enabled
 ENCODER_MAP_ENABLE = yes	# Ability to use Keymap with encoder
-LTO_ENABLE = yes 			# Optimise
+LTO_ENABLE = no 		# Optimise
 
 WS2812_DRIVER = vendor
 SERIAL_DRIVER = vendor
